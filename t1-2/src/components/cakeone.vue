0751c7fe2abbc590e8bc24f499f5221010f1b51e
王敏 <569049370@qq.com>
2020-04-07 14:37:41 +0800

    create component cakeone

---
+++ t1-2/src/components/cakeone.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>I am a component cakeone</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
